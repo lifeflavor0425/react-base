fe73c07d696410a90164e248774b8568876f04c2
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2024-01-09 14:47:51 +0900

    Revert "Revert "update: functional component change""
    This reverts commit 0a2f359b61d7d2490788a392205e1a55d5c6128a.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,41 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.css";
-import { ExpenseForm } from "./components/ExpenseForm";
-import { ExpenseList } from "./components/ExpenseList";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expenses: [
-        { id: 1, charge: "렌트비", amount: 1600 },
-        { id: 2, charge: "교통비", amount: 400 },
-        { id: 3, charge: "식비", amount: 1200 },
-      ],
-    };
-  }
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
-  handelDelete = (id) => {
-    const newExpense = this.state.expenses.filter(
-      (expense) => expense.id !== id
-    );
-    this.setState({ expenses: newExpense });
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, charge: "렌트비", amount: 1600 },
+    { id: 2, charge: "교통비", amount: 400 },
+    { id: 3, charge: "식비", amount: 1200 },
+  ]);
+
+  const handelDelete = (id) => {
+    const newExpense = expenses.filter((expense) => expense.id !== id);
+    setExpenses(newExpense);
   };
 
-  render() {
-    return (
-      <main className="main-container">
-        <h1>예산 계산기</h1>
-        <div
-          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
-        >
-          {/* {ExpenseForm} */}
-          <ExpenseForm />
-        </div>
-        <div
-          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
-        >
-          {/* {ExpenseItem} */}
-          <ExpenseList
-            initialExpenses={this.state.expenses}
-            handelDelete={this.handelDelete}
-          />
-        </div>
-        <div
-          style={{ display: "flex", justifyContent: "end", marginTop: "1rem" }}
-        >
-          <p style={{ fontSize: "2rem" }}>
-            총지출 :<span>원</span>
-          </p>
-        </div>
-      </main>
-    );
-  }
-}
+  return (
+    <main className="main-container">
+      <h1>예산 계산기</h1>
+      <div style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}>
+        {/* {ExpenseForm} */}
+        <ExpenseForm />
+      </div>
+      <div style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}>
+        {/* {ExpenseItem} */}
+        <ExpenseList initialExpenses={expenses} handelDelete={handelDelete} />
+      </div>
+      <div
+        style={{ display: "flex", justifyContent: "end", marginTop: "1rem" }}
+      >
+        <p style={{ fontSize: "2rem" }}>
+          총지출 :<span>원</span>
+        </p>
+      </div>
+    </main>
+  );
+};
 
 export default App;
 
