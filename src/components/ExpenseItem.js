0a2f359b61d7d2490788a392205e1a55d5c6128a
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2024-01-09 14:41:35 +0900

    Revert "update: functional component change"
    This reverts commit 6f647dadeb518ec708daaa3a3f3ea996c4be4e4a.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ExpenseItem.css";
 import { MdEdit, MdDelete } from "react-icons/md";
-
-const ExpenseItem = ({ expense, handelDelete }) => {
-  return (
-    <li className="item">
-      <div className="info">
-        <span className="expense">{expense.charge}</span>
-        <span className="amount"> {expense.amount}</span>
-      </div>
-      <div>
-        <button className="edit-btn">
-          <MdEdit />
-        </button>
-        <button
-          className="clear-btn"
-          onClick={() => {
-            handelDelete(expense.id);
-          }}
-        >
-          <MdDelete />
-        </button>
-      </div>
-    </li>
-  );
-};
+export class ExpenseItem extends Component {
+  render() {
+    return (
+      <li className="item">
+        <div className="info">
+          <span className="expense">{this.props.expense.charge}</span>
+          <span className="amount"> {this.props.expense.amount}</span>
+        </div>
+        <div>
+          <button className="edit-btn">
+            <MdEdit />
+          </button>
+          <button
+            className="clear-btn"
+            onClick={() => {
+              this.props.handelDelete(this.props.expense.id);
+            }}
+          >
+            <MdDelete />
+          </button>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default ExpenseItem;
